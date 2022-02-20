3a1978835d6611053a114985459f61d18265b09f
Tomasz <stonka123@gmail.com>
2022-02-20 19:43:12 +0100

    about me section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-// Can be used with JavaScript
-// -----------------------------
 const brgBtn = document.querySelector('.menu-icon')
 const nav = document.querySelector('.nav')
 const logoIcon = document.querySelector('.logo-icon')
+const navItem = document.querySelectorAll('.nav__items-item')
+const checkedIcon = document.querySelector('.menu-icon__cheeckbox')
 
 const showNav = () => {
 	nav.classList.toggle('nav-active')
 	logoIcon.classList.toggle('logo-icon--active')
+
+	navItem.forEach(el => {
+		el.addEventListener('click', () => {
+			nav.classList.remove('nav-active')
+			logoIcon.classList.remove('logo-icon--active')
+			nav.classList.remove('nav-active')
+			checkedIcon.checked = false
+		})
+	})
 }
 
 brgBtn.addEventListener('click', showNav)
