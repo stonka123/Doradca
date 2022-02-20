15275a56583b24b8595a308d2b858d5c134fe299
Tomasz <stonka123@gmail.com>
2022-02-20 12:01:51 +0100

    header

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 // -----------------------------
 const brgBtn = document.querySelector('.menu-icon')
 const nav = document.querySelector('.nav')
+const logoIcon = document.querySelector('.logo-icon')
 
 const showNav = () => {
 	nav.classList.toggle('nav-active')
+	logoIcon.classList.toggle('logo-icon--active')
 }
 
 brgBtn.addEventListener('click', showNav)
